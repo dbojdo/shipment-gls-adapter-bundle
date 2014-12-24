073f051c0985d0fe60e845523bf49fa78c2bdd9f
Daniel Bojdo <daniel.bojdo@dxi.eu>
2014-12-24 12:43:01 +0000

    [new] 1st commit

---
+++ src/WebitShipmentGlsAdapterBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * WebitShipmentGlsAdapterBundle.php
+ *
+ * @author dbojdo - Daniel Bojdo <daniel.bojdo@dxi.eu>
+ * Created on Dec 24, 2014, 12:09
+ * Copyright (C) DXI Ltd
+ */
+
+namespace Webit\Bundle\ShipmentGlsAdapterBundle;
+
+/**
+ * Class WebitShipmentGlsAdapterBundle
+ * @package Webit\Bundle\ShipmentGlsAdapterBundle
+ */
+class WebitShipmentGlsAdapterBundle extends Bundle
+{
+
+}
